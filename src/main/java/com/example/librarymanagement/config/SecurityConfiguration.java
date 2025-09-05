6111505feeabbdbb7648b8ed4a3fe65e041d4cb7
MCET-CPG-JAVA <mcetcgp@gmail.com>
2025-09-05 12:53:06 +0530

    LDAP

--- src/main/java/com/example/librarymanagement/config/SecurityConfiguration.java
+++ src/main/java/com/example/librarymanagement/config/SecurityConfiguration.java
@@ -1,14 +1,18 @@
 package com.example.librarymanagement.config;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.ldap.DefaultSpringSecurityContextSource;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -47,4 +51,43 @@ public class SecurityConfiguration {
 
 		return http.build();
 	}
+
+    @Bean
+    public DefaultSpringSecurityContextSource contextSource() {
+        DefaultSpringSecurityContextSource contextSource =
+                new DefaultSpringSecurityContextSource(
+                        Arrays.asList("ldap://localhost:8389/"),
+                        "dc=springframework,dc=org"
+                );
+        contextSource.setUserDn("cn=admin,dc=springframework,dc=org");
+        contextSource.setPassword("admin");
+        return contextSource;
+    }
+
+    /*
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder authBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+        authBuilder
+                .ldapAuthentication()
+                .userDnPatterns("uid={0},ou=people")
+                .contextSource(contextSource())
+                .passwordCompare()
+                .passwordAttribute("userPassword");
+        return authBuilder.build();
+    }
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().fullyAuthenticated()
+                )
+                .formLogin();
+        return http.build();
+    }
+    */
+
+
 }
