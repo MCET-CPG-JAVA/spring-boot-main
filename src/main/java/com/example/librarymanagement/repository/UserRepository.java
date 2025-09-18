2259644eda1321c191c693e5e84561a629fe3d03
MCET-CPG-JAVA <mcetcgp@gmail.com>
2025-09-18 12:49:50 +0530

    Update UserRepository.java

--- src/main/java/com/example/librarymanagement/repository/UserRepository.java
+++ src/main/java/com/example/librarymanagement/repository/UserRepository.java
@@ -3,9 +3,15 @@ package com.example.librarymanagement.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import com.example.librarymanagement.modal.User;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
 	Optional<User> findByEmail(String email);
+
+	@Modifying
+	@Query(nativeQuery = true, value = "update users set  role_id= ?2 where id = ?1 ")
+	void updateRole(Integer id, Integer roleId);
 }
